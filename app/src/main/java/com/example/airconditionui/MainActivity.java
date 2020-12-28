65519652dc06171b7317184a7400e2cafe479e81
JasonChristo99 <38409798+JasonChristo99@users.noreply.github.com>
2020-12-28 17:57:18 +0200

    init project

---
+++ app/src/main/java/com/example/airconditionui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.airconditionui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/* This is the main activity of the AC UI application */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
