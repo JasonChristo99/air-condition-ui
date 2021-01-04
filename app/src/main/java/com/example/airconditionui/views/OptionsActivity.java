d7d27aaf355ee6709e750480b12e6651f5f79b96
JasonChristo99 <38409798+JasonChristo99@users.noreply.github.com>
2021-01-04 03:13:08 +0200

    init material style

--- app/src/main/java/com/example/airconditionui/views/OptionsActivity.java
+++ app/src/main/java/com/example/airconditionui/views/OptionsActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.airconditionui.R;
 import com.example.airconditionui.views.options.OptionFanSpeedActivity;
@@ -21,6 +22,7 @@ public class OptionsActivity extends AppCompatActivity implements OptionsRecycle
 
     RecyclerView optionsRecyclerView;
     OptionsRecyclerViewAdapter optionsRecyclerViewAdapter;
+    Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +40,19 @@ public class OptionsActivity extends AppCompatActivity implements OptionsRecycle
 
         optionsRecyclerViewAdapter = new OptionsRecyclerViewAdapter(this, options);
         optionsRecyclerView = findViewById(R.id.optionsRecView);
+        backBtn = findViewById(R.id.optBackButton);
 
         optionsRecyclerView.setAdapter(optionsRecyclerViewAdapter);
         optionsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         optionsRecyclerViewAdapter.setClickListener(this);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
     }
 
